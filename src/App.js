b05e788185e8f050e4cc27aff3a1f6e30ae2065d
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2022-11-21 13:38:33 +0100

    estado y evento click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo!
-        </p>
+        {/* Componente propio */}
+        <Greeting name="Javi"></Greeting>
       </header>
     </div>
   );
--- src/components/pure/greeting.jsx
+++ src/components/pure/greeting.jsx
@@ -4,17 +4,37 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Greeting extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            age: 33
+        }
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>
+                Hola { this.props.name } !
+            </h1>
+            <h2>
+                Tu edad es de { this.state.age } años.
+            </h2>
+            <div>
+                <button onClick={this.birthday}>Birthday</button>
+            </div>
+        </div>
+        )
+    }
+
+    birthday = () => {
+        this.setState(prevState => ({ age: prevState.age + 1 }))
+    }
 }
 
 Greeting.protoTypes = {
-
+    name: PropTypes.string
 };
 
 export default Greeting;

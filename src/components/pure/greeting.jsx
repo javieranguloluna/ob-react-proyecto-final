35cc0fb27bee8ece4f14aa7a7e365b8ff5d00c8e
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2022-11-21 13:03:54 +0100

    class component bed

---
+++ src/components/pure/greeting.jsx
@@ -0,0 +1,20 @@
+// COMPONENTE TIPO CLASE -> rcc
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Greeting extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+Greeting.protoTypes = {
+
+};
+
+export default Greeting;

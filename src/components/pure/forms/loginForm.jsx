eded7feb36d82653370fd3ef32ea8ee221e877e7
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2022-11-23 14:27:28 +0100

    forms init

---
+++ src/components/pure/forms/loginForm.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export const LoginForm = () => {
+
+    const initialCredentials = [
+        {
+            user: '',
+            password: ''
+        }
+    ]
+
+    const [credentials, setCredentials] = useState(initialCredentials)
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
---
+++ src/components/pure/forms/registerForm.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+export const RegisterForm = () => {
+
+    const initialData = [
+        {
+            user: '',
+            name: '',
+            email: '',
+            password: ''
+        }
+    ]
+
+    const [data, setData] = useState(initialData)
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
